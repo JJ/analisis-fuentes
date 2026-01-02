ad7dc80bdc7f7326380b2ecaf13a4090f04a7f24
JJ Merelo <jjmerelo@gmail.com>
2026-01-02 12:40:48 +0100

    closes #1 graph done

---
+++ graph_layout.R
@@ -0,0 +1,24 @@
+historiografia_data <- read.csv("data/grafo.csv", header = TRUE, stringsAsFactors = FALSE)
+
+library(igraph)
+g <- graph_from_data_frame(historiografia_data, directed = FALSE)
+plot(g, vertex.label.cex=0.7, vertex.size=5, edge.arrow.size=0.5)
+
+V(g)$degree <- degree(g, mode="all")
+V(g)$betweenness <- betweenness(g, directed=FALSE)
+
+plot(g, vertex.size=V(g)$betweenness/100, vertex.label.cex=0.7, edge.arrow.size=0.5)
+
+# Extract main connected component from g
+
+components <- components(g)
+main_component <- which.max(components$csize)
+vertices_in_main_component <- V(g)[components$membership == main_component]
+g_main <- induced_subgraph(g, vertices_in_main_component)
+
+layout <- layout_with_kk(g_main)
+plot(g_main, vertex.size=V(g_main)$betweenness/200, vertex.color = rainbow(10, .8, .8, alpha= .8), layout=layout, vertex.label.cex=0.7, edge.arrow.size=0.5, alpha = .8)
+
+
+
+
