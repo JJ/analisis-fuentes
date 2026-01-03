b0bfa6f2809086391668775d87c663cc040b3388
JJ Merelo <jjmerelo@gmail.com>
2026-01-03 09:33:29 +0100

    #1 con edge betweenness

--- graph_layout.R
+++ graph_layout.R
@@ -7,6 +7,7 @@ plot(g, vertex.label.cex=0.7, vertex.size=5, edge.arrow.size=0.5)
 
 V(g)$degree <- degree(g, mode="all")
 V(g)$betweenness <- betweenness(g, directed=FALSE)
+E(g)$edge_betweenness <- edge_betweenness(g)
 
 plot(g, vertex.size=V(g)$betweenness/100, vertex.label.cex=0.7, edge.arrow.size=0.5)
 
@@ -16,8 +17,11 @@ vertices_in_main_component <- V(g)[components$membership == main_component]
 g_main <- induced_subgraph(g, vertices_in_main_component)
 
 layout <- layout_with_kk(g_main)
-plot(g_main, vertex.size=V(g_main)$betweenness/200, vertex.color = rainbow(10, .8, .8, alpha= .8), layout=layout, vertex.label.cex=0.7, edge.arrow.size=0.5, alpha = .8)
-
+V(g_main)$label.cex <- 1+V(g_main)$degree/10
 png("figures/graph_layout.png", width=2400, height=1600)
+plot(g_main, vertex.size=2+V(g_main)$betweenness/200, vertex.color = rainbow(10, .8, .8, alpha= .8),
+     edge.width=1+E(g_main)$edge_betweenness/100,
+     layout=layout, alpha = .8)
 
+dev.off()
 
